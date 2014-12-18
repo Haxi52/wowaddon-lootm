eb699b03af4013025de06c049af12757c9ce9b26
haxi32 <jay.bourgeois7@gmail.com>
2014-12-17 18:07:33 -0600

    Updated comms to handle multi part messages Updated comms to handle unloaded item data Updated improvement raiting calculations to handle weapons.

--- LootM/LootM.lua
+++ LootM/LootM.lua
@@ -2,7 +2,7 @@ seterrorhandler(print);
 
 LootMFrames = { };
 LootMEvents = { };
-LootMItemEvaluator = {};
+LootMItemEvaluator = { };
 
 local LootM = CreateFrame("FRAME", "LootM"), { };
 
@@ -18,13 +18,13 @@ function LootMEvents:LOOT_OPENED(...)
             table.insert(lootTable, itemLink);
         end
     end
-    if (LootMItemEntries.IsNewLoot(lootTable)) then 
+    if (LootMItemEntries.IsNewLoot(lootTable)) then
         LootMComms.NewLoot(lootTable);
     end
 end
 
 function LootMEvents:LOOT_CLOSED(...)
-    
+
 end
 
 function LootMEvents:CHAT_MSG_ADDON(...)
@@ -63,120 +63,194 @@ function RegisterFrame(frame)
 end
 
 
-LootMItemEvaluator = (function ()
+LootMItemEvaluator =( function()
 
     local inventoryMap = {
-    ["INVTYPE_AMMO"]=0,
-    ["INVTYPE_HEAD"]=1,
-    ["INVTYPE_NECK"]=2,
-    ["INVTYPE_SHOULDER"]=3,
-    ["INVTYPE_BODY"]=4,
-    ["INVTYPE_CHEST"]=5,
-    ["INVTYPE_ROBE"]=5,
-    ["INVTYPE_WAIST"]=6,
-    ["INVTYPE_LEGS"]=7,
-    ["INVTYPE_FEET"]=8,
-    ["INVTYPE_WRIST"]=9,
-    ["INVTYPE_HAND"]=10,
-    ["INVTYPE_FINGER"]={11,12},
-    ["INVTYPE_TRINKET"]={13,14},
-    ["INVTYPE_CLOAK"]=15,
-    ["INVTYPE_THROWN"]=18,
-    ["INVTYPE_RELIC"]=18,
-    ["INVTYPE_TABARD"]=19,
-
-    -- weapon slots
-    -- 2 handers
-    ["INVTYPE_2HWEAPON"]={16,17}, -- 2 handers/staves
-    ["INVTYPE_RANGED"]=18, -- bows (2h)
-    -- 1 handers
-    ["INVTYPE_WEAPON"]={16,17}, -- one handed
-    ["INVTYPE_WEAPONMAINHAND"]={16,17}, -- only right handded
-    ["INVTYPE_RANGEDRIGHT"]=18, -- main wands
-    -- off handers
-    ["INVTYPE_SHIELD"]= 17, -- duh
-    ["INVTYPE_WEAPONOFFHAND"]= 17, -- only left hand?
-    ["INVTYPE_HOLDABLE"]= 17, -- off hand
-
-    -- Token texutres TODO: Lookup the real texture files
-    ["icons/chest_armor_token"] = 5,
-    ["icons/hands_armor_token"] = 10,
-    ["icons/head_armor_token"] = 1,
-    ["icons/legs_armor_token"] = 7,
-    ["icons/sholders_armor_token"] = 3,
+        ["INVTYPE_AMMO"] = 0,
+        ["INVTYPE_HEAD"] = 1,
+        ["INVTYPE_NECK"] = 2,
+        ["INVTYPE_SHOULDER"] = 3,
+        ["INVTYPE_BODY"] = 4,
+        ["INVTYPE_CHEST"] = 5,
+        ["INVTYPE_ROBE"] = 5,
+        ["INVTYPE_WAIST"] = 6,
+        ["INVTYPE_LEGS"] = 7,
+        ["INVTYPE_FEET"] = 8,
+        ["INVTYPE_WRIST"] = 9,
+        ["INVTYPE_HAND"] = 10,
+        ["INVTYPE_FINGER"] = { 11, 12 },
+        ["INVTYPE_TRINKET"] = { 13, 14 },
+        ["INVTYPE_CLOAK"] = 15,
+        ["INVTYPE_THROWN"] = 18,
+        ["INVTYPE_RELIC"] = 18,
+        ["INVTYPE_TABARD"] = 19,
     };
 
+    local tokenIcons = {
+        ["icons/chest_armor_token"] = 5,
+        ["icons/hands_armor_token"] = 10,
+        ["icons/head_armor_token"] = 1,
+        ["icons/legs_armor_token"] = 7,
+        ["icons/sholders_armor_token"] = 3,
+    };
+
+    local offHandItemType, twoHandItemType, oneHandItemType = "offhand", "twohand", "onehand";
+    local offHandItems = {
+        ["INVTYPE_SHIELD"] = 17,
+        ["INVTYPE_WEAPONOFFHAND"] = 17,
+        ["INVTYPE_HOLDABLE"] = 17,-- off hand
+    };
+
+    local twoHandItems = {
+        ["INVTYPE_2HWEAPON"] = { 16, 17 },
+        ["INVTYPE_RANGED"] = { 16, 17 },-- bows (2h)
+    };
+
+    local oneHandItems = {
+        ["INVTYPE_WEAPON"] = { 16, 17 },
+        ["INVTYPE_WEAPONMAINHAND"] = 16,
+        ["INVTYPE_RANGEDRIGHT"] = 16,-- main wands
+    };
+
+    local function getPlayerInventoryItem(slotId)
+        GetInventoryItemLink('player', slotId)
+    end
     -- the premis on thsi one is that a token is misc/junk with specific textures for each slot
     -- as long as the game keeps that, we can assume these will be tokens
     local function getItemTokenSlot(itemType, itemSubType, itemTexture)
         if (itemType ~= 'Miscellaneous' or itemSubType ~= 'Junk') then return nil; end
-        return inventoryMap[string.lower(itemTexture)];
+        return tokenIcons[string.lower(itemTexture)];
+    end
+
+    local function getPlayerWeaponRelatedItems(itemLink, itemEquipLocation)
+        -- this is rather complicated. The gloal here is readability, so have some repetitive code
+        local playerItems={};
+        local lootItemType, playerItemType;
+        local mainHandSlot, offHandSlot = 16, 17;
+        local offHandItem = getPlayerInventoryItem(offHandSlot);
+
+        if (offHandItems[itemEquipLocation]) then -- is the looted item an offhand?
+            lootItemType = offHandItemType;
+            if (not offHandItem) then -- loot item is an offhand, but equipped 2 hander
+                playerItemType = twoHandItemType;
+                playerItems[1] = getPlayerInventoryItem(mainHandSlot);
+            else -- loot item is offhand and has equipped offhand/one hander
+                playerItemType = offHandItemType;
+                playerItems[1] = offHandItem;
+            end
+        elseif (oneHandItems[itemEquipLocation]) then -- is the looted item a one hander?
+            lootItemType = oneHandItemType;
+            if (not offHandItem) then -- looted item is 1h but have equipped a 2h
+                playerItemType = twoHandItemType;
+                playerItems[1] = getPlayerInventoryItem(mainHandSlot);
+            else -- item loot is 1 hand and have equipped 1handers 
+                playerItemType = oneHandItemType;
+                playerItems[1] = getPlayerInventoryItem(mainHandSlot);
+                playerItems[2] = offHandItem;
+            end
+        elseif (twoHandItems[itemEquipLocation]) then -- is the looted item a two hander?
+            lootItemType = twoHandItemType;
+            if (not offHandItem) then -- loot item is 2h and has equipped a 2h
+                playerItemType = twoHandItemType;
+                playerItems[1] = getPlayerInventoryItem(mainHandSlot);
+            else -- loot item is 2h and has equpped 1hx2 or mh,oh
+                playerItemType = oneHandItemType;
+                playerItems[1] = getPlayerInventoryItem(mainHandSlot);
+                playerItems[2] = offHandItem;
+            end
+        else
+            return nil;
+        end
+        return playerItems, lootItemType, playerItemType;
     end
 
     local function getPlayerRelatedItems(itemLink)
-        local itemName, itemLink, itemRarity, itemLevel, _, itemType, itemSubType, _, itemEquipLocation, itemTexture =
-            GetItemInfo(itemLink);
+        local _, _, _, _, _, itemType, itemSubType, _, itemEquipLocation, itemTexture = GetItemInfo(itemLink);
+
         -- check to see if the item is a token, then assing the appropreate inventory slot for said token
         local playerSlot = getItemTokenSlot(itemType, itemSubType, itemTexture);
+
         if (not playerSlot) then
-            playerSlot = inventoryMap[itemEquipLocation];
-        end
-        -- weapons are handled differently
-        if (itemType == 'Weapon') then
+            -- weapons are handled differently
+            local a, b, c = getPlayerWeaponRelatedItems(itemLink, itemEquipLocation);
+            if (a) then
+                return a, b, c;
+            else
+                playerSlot = inventoryMap[itemEquipLocation];
+            end
         end
 
         local dataType = type(playerSlot);
-        local playerItems = {};
+        local playerItems = { };
 
         if (dataType == 'number') then
-            table.insert(playerItems, GetInventoryItemLink('player', playerSlot));
+            table.insert(playerItems, getPlayerInventoryItem(playerSlot));
         elseif (dataType == 'table') then
-            for k,v in pairs(playerSlot) do
-                local equippedItemLink = GetInventoryItemLink('player', v);
+            for k, v in pairs(playerSlot) do
+                local equippedItemLink = getPlayerInventoryItem(v);
                 if (equippedItemLink) then
                     table.insert(playerItems, equippedItemLink);
                 end
             end
         end
         -- returns item links for the equpped item and a bool if the item is a weapon
-        return playerItems, (playerSlot == 16 or playerSlot[1] == 16);
+        return playerItems, nil, nil;
     end;
 
     local function getItemValue(itemLink, weightTable)
         local itemStats = GetItemStats(itemLink);
-        local itemValue =0;
-        for k,v in pairs(statTable) do
+        local itemValue = 0;
+        for k, v in pairs(statTable) do
             local weight = weightTable[k];
-            if (weight and weight >=0) then
-                itemValue = itemValue + (v * weight);
+            if (weight and weight >= 0) then
+                itemValue = itemValue +(v * weight);
             end
         end
         return itemValue;
     end;
 
-    local function getImprovementRating(equippedValue, newValue)
-        local value =0;
+    local function getItemImprovementRating(equippedValue, newValue)
+        local value = 0;
         if (equippedValue > 0 and newValue > 0) then
-            value = (newValue - equippedValue) / equippedValue;
+            value =(newValue - equippedValue) / equippedValue;
             value = math.max(0, value);
             value = value * 100;
         end
         return value;
     end
 
-    return {
-        GetPlayerItemDetails = function (itemLink)
-            
-            local playerItems, isWeapon = getPlayerRelatedItems(itemLink);
-            local improvementRating =0;
-            for k,v in pairs(playerItems) do
-                local old = getItemValue(itemLink, LootMStatWeights[1]);
-                local new = getItemValue(v, LootMStatWeights[1]);
-                improvementRating = math.max(
-                    getImprovementRating(old, new),
-                    improvementRating);
+    local function calculateImprovementRating(itemLink, playerItems, statWeights, lootItemType, playerItemType)
+        local improvementRating = 0;
+        local new = getItemValue(itemLink, statWeights);
+        -- if the items are not weapons, or the weapons are the same
+        -- the comparison is straight forward
+        -- lootItemType is set in function (getPlayerWeaponRelatedItems)
+        if (not lootItemType or (lootItemType == playerItemType)) then
+            for k, v in pairs(playerItems) do
+                local old = getItemValue(v, statWeights);
+                improvementRating = math.max(getImprovementRating(old, new), improvementRating);
             end
+        elseif (lootItemType == twoHandItemType) then -- not the same, so player type is 1 or offhand
+            local equippedValue = 
+                getItemValue(playerItems[1], statWeights) +
+                getItemValue(playerItems[2], statWeights); -- add the value of both equipped weapons to compare against 2h
+            improvementRating = getImprovementRating(equippedValue, new);
+        elseif (lootItemType == oneHandItemtype or lootItemType == offHandItemType) then
+            -- player should have a 2h
+            local equippedValue = getItemValue(playerItems[1], statWeights);
+            improvementRating = getImprovementRating(equippedValue, (new * 2));
+        end
+        return math.max(0, improvementRating);
+    end
 
+    return {
+        GetPlayerItemDetails = function(itemLink)
+            -- find out which items we are interested in comparing against the looted item.
+            local playerItems, lootItemType, playerItemType = getPlayerRelatedItems(itemLink);
+            -- calculate an improvement rating agaist the player equipped items
+            -- TODO: Grab a stat weight table appropreate for the player
+            local improvementRating = 
+                calculateImprovementRating(itemLink, playerItems, LootMStatWeights[1], lootItemType, playerItemType);
             return playerItems, improvementRating;
         end,
         GetItemValue = function(itemLink, weightTable)
@@ -184,14 +258,14 @@ LootMItemEvaluator = (function ()
         end,
     };
 
-end)();
+end )();
 
 -- LootM_Show
 -- /lootm
 SLASH_LOOTM1 = '/lootm';
-SlashCmdList["LOOTM"] = function (message)
+SlashCmdList["LOOTM"] = function(message)
     if (string.sub(message, 1, 4) == 'test') then
-        LootMComms.NewLoot({ string.sub(message, 5) });
+        LootMComms.NewLoot( { string.sub(message, 5) });
     elseif (string.sub(message, 1, 4) == 'need') then
         local x = LootMItemEntries.GetItems();
         LootMItemEntries.SetPlayerRoll(x[1], 'TheNewGuy', '1', LootMItemEvaluator.GetPlayerItemDetails(x[1]));
@@ -207,13 +281,13 @@ end;
 -- http://stackoverflow.com/a/15706820
 function spairs(t, order)
     -- collect the keys
-    local keys = {}
-    for k in pairs(t) do keys[#keys+1] = k end
+    local keys = { }
+    for k in pairs(t) do keys[#keys + 1] = k end
 
     -- if order function given, sort by it by passing the table and keys a, b,
-    -- otherwise just sort the keys 
+    -- otherwise just sort the keys
     if order then
-        table.sort(keys, function(a,b) return order(t, a, b) end)
+        table.sort(keys, function(a, b) return order(t, a, b) end)
     else
         table.sort(keys)
     end
@@ -237,10 +311,11 @@ end
 -- assign loot
 -- reset loot session (loot master button)
 -- spirit only to healers, bonus armor only to tanks
+-- Improvement ratings on trinkets?
 
 -- for debugging
 function PrintTable(t)
-    for k,v in pairs(t) do
-        print(k..": "..v);
+    for k, v in pairs(t) do
+        print(k .. ": " .. v);
     end
 end
--- LootM/LootMComms.lua
+++ LootM/LootMComms.lua
@@ -1,43 +1,99 @@
 -- fortunatly the communication requirements for this are fairly straight forward, so the formatting is rutamentry
 
-LootMComms = (function ()
+LootMComms =( function()
 
-    local raidMessageType = "OFFICER"; -- changed for testing
-    --local raidMessageType = "RAID";
+    local raidMessageType = "OFFICER";
+    -- changed for testing
+    -- local raidMessageType = "RAID";
     local newLootPrefix = 'LootMNew';
-    local newLootStartPrefix = 's';
-    local newLootItemPrefix = 'i';
-    local newLootEndPrefix = 'e';
     local rollPrefix = 'LootMRoll';
     local awardPrefix = 'LootMAward';
+    local newLootMessageSpool = {};
+    local pendingMessages = { };
 
+    local function processNewLootMessage(itemCount)
+        -- deturmins if the new loot spool has all the items expected then calls out for the items to be loaded
+        if (not newLootMessageSpool) then
+            newLootMessageSpool = {};
+            return;
+        end
+
+        local i =0;
+        for i in pairs(newLootMessageSpool) do
+            i = i + 1;
+        end
+        if (i ~= itemCount) then
+            return;
+        end
+
+        LootMItemEntries.Hide();
+        for k,itemLink in pairs(newLootMessageSpool) do
+            LootMItemEntries.ShowItem(itemLink);    
+        end    
+        LootMItemEntries.Show();
+        print('[LootM] Staring new loot session. Take a gandar');
+    end
+
+    
     -- a dictionary of message prefixes (used as message type) and funtions to handle/parse the message
     -- see below for the format of each message type
     local chatMessageHandlers = {
-        [newLootPrefix] = function (message, sender)
-            local prefix = string.sub(message, 1, 1);
-            if (prefix == newLootStartPrefix) then LootMItemEntries.Hide(); end;
-            if (prefix == newLootEndPrefix) then
-                LootMItemEntries.Show();
-                print('[LootM] Staring new loot session. Take a gandar');
+        [newLootPrefix] = function(message, sender)
+            local parsedMessage = {};
+            for i in string.gmatch("([^;]+)") do
+                table.insert(parsedMessage, i);
+            end
+            local messageCount, messageIndex, itemLink = unpack(parsedMessage);
+            if (newLootMessageSpool[messageIndex]) then
+                -- this shoudnt happen
+                newLootMessageSpool = {};
+            end
+
+            -- if the item is not loaded, pend the message untill the server can get us info about the item
+            if (not GetItemInfo(itemLink)) then
+                table.insert(pendingMessages,
+                {
+                    MessageType = newLootPrefix,
+                    Message = message,
+                    Sender = sender,
+                });
                 return;
-            end;
-            LootMItemEntries.ShowItem(string.sub(message, 2));
+            end
+
+            newLootMessageSpool[messageIndex] = itemLink;
+            processNewLootMessage(messsageCount);
         end,
-        [rollPrefix] = function (message, sender) 
-            local roll = string.sub(message, 1, 1);
-            local itemsTable = {}; 
-            local firstItem = nil;
-            for item in string.gmatch(string.sub(message, 2), '([^;]+)') do
-                if (not firstItem) then 
-                    firstItem = item; 
-                else
-                    table.insert(itemsTable, item);
+        [rollPrefix] = function(message, sender)
+            local parsedMessage = {};
+            for i in string.gmatch("([^;]+)") do
+                table.insert(parsedMessage, i);
+            end
+            local rollId, itemLink, improvementRating, playerItems = (function(a, b, c, ...)
+                return a,b,c,{...};
+            end)(unpack(parsedMessage));
+
+            -- We are not short-circuting this logic because we want to make sure all
+            -- items are being requested from the server.
+            local requiresItemLoad = (not GetItemInfo(itemLink));
+            for k,item in pairs(playerItems) do
+                if (not GetItemInfo(item)) then
+                    requiresItemLoad = true;
                 end
             end
-            LootMItemEntries.SetPlayerRoll(firstItem, sender, roll, itemsTable);
+
+            if (requiresItemLoad) then
+                table.insert(pendingMessages,
+                {
+                    MessageType = rollPrefix,
+                    Message = message,
+                    Sender = sender,
+                });
+                return;
+            end
+
+            LootMItemEntries.SetPlayerRoll(itemLink, sender, rollId, playerItems, improvementRating);
         end,
-        [awardPrefix] = function (message, sender) return; end,
+        [awardPrefix] = function(message, sender) return; end,
     };
 
     -- main event handler which dispatches the message to a handler based on prefix
@@ -46,36 +102,51 @@ LootMComms = (function ()
         if (f) then f(message, sender); end;
     end;
 
+    local itemsLoaded = function()
+        -- swap out the table so we can process the messages
+        local messages, pendingMessages = pendingMessages, {};
+        -- re call into each handler, if the item is still not loaded it will reque the message.
+        for k,v in pairs(messages) do
+            local f = chatMessageHandlers[v.MessageType];
+            if (f) then 
+                f(v.Message, v.Sender); 
+            end
+        end
+    end
+
+    for k,v in pairs(chatMessageHandlers) do
+        RegisterAddonMessagePrefix(k);
+    end
+
     return {
         -- signals to raid members new lootable items are available from the loot master
-        -- Message format: [prefix][itemlink]
-        -- where [prefix] is a single character (see above for values)
-        -- single the start of a list, a list item, and finally a terminator
-        -- [itemlink] taken as the remainder of the message string
         NewLoot = function(lootTable)
-            local messagePrefix = newLootStartPrefix;
-            for i,v in ipairs(lootTable) do
-                SendAddonMessage(newLootPrefix, messagePrefix .. v, raidMessageType);
-                messagePrefix = newLootItemPrefix;
+            local lootCount = #lootTable;
+            for i, v in ipairs(lootTable) do
+                -- [total messages];[this message index];[item link]
+                SendAddonMessage(
+                    newLootPrefix, 
+                    table.concat({lootCount, i, v}, ";"),
+                    raidMessageType);
             end
-            messagePrefix = newLootEndPrefix;
-            SendAddonMessage(newLootPrefix, messagePrefix, raidMessageType);
         end,
+
         -- singles a player's roll selection on a item being looted
-        -- format is [RoleId][itemlink][playeritems][improvementraiting]
-        -- rollid represents the roll action taken (need/greed/etc)
-        -- itemlink is the item link of the looted item being rolled on (recieved from newloot)
-        -- playeritems ';' delemited list of items the player has currently equipped
-        Roll = function(rollId, itemLink, playerItems) 
-            local playerItemsString = '';
+        Roll = function(rollId, itemLink, playerItems, improvementRating)
+
+            local message = { rollId, itemLink, improvementRating };
             if (playerItems) then
-                playerItemsString = ';' .. table.concat(playerItems, ';');
+                for k,v in pairs(playerItems) do
+                    table.insert(message, v);
+                end
             end
-            SendAddonMessage(rollPrefix, rollId .. itemLink .. playerItemsString, raidMessageType);
+            -- [rollid];[item being rolled on];[improvementrating];[table...of player equipped items]
+            SendAddonMessage(rollPrefix, table.concat(message, ';'), raidMessageType);
         end,
         -- TODO: Implement award
         Award = function(itemLink, awardee) return; end,
-        MessageRecieved = chatMessageEvent, -- proxy to publically expose the handler
+        MessageRecieved = chatMessageEvent,-- proxy to publically expose the handler
+        ItemsLoaded = ItemsLoaded, -- proxy for even when item data is received from server
     };
 
-end)();
+end )();
