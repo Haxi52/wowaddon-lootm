3bd40da6bd31621f5d4ba40d2daf27649434677b
haxi32 <jay.bourgeois7@gmail.com>
2014-12-24 17:46:56 -0600

    Started module to check player useable items

---
+++ LootM/ItemRollCheck.lua
@@ -0,0 +1,92 @@
+local classInfo = {
+
+    -- Death Knight
+    -- Blood
+    [250] = { ArmorType = "Plate", Weapons = {}, PrimaryStat = "Strength" },
+    -- Frost
+    [251] = { ArmorType = "Plate", Weapons = {}, PrimaryStat = "Strength" },
+    -- Unholy
+    [252] = { ArmorType = "Plate", Weapons = {}, PrimaryStat = "Strength" },
+
+    -- Druid
+    -- Balance
+    [102] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Intellect" },
+    -- Feral Combat
+    [103] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Agility" },
+    -- Guardian
+    [104] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Agility" },
+    -- Restoration
+    [105] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Intellect" },
+
+    -- Hunter
+    -- Beast Mastery
+    [253] = { ArmorType = "Mail", Weapons = {}, PrimaryStat = "Agility" },
+    -- Marksmanship
+    [254] = { ArmorType = "Mail", Weapons = {}, PrimaryStat = "Agility" },
+    -- Survival
+    [255] = { ArmorType = "Mail", Weapons = {}, PrimaryStat = "Agility" },
+    
+    -- Mage
+    -- Arcane
+    [62] = { ArmorType = "Cloth", Weapons = {}, PrimaryStat = "Intellect" },
+    -- Fire
+    [63] = { ArmorType = "Cloth", Weapons = {}, PrimaryStat = "Intellect" },
+    -- Frost
+    [64] = { ArmorType = "Cloth", Weapons = {}, PrimaryStat = "Intellect" },
+    
+    -- Monk
+    -- Brewmaster
+    [268] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Agility" },
+    -- Windwalker
+    [269] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Agility" },
+    -- Mistweaver
+    [270] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Agility" },
+    
+    -- Paladin
+    -- Holy
+    [65] = { ArmorType = "Plate", Weapons = {}, PrimaryStat = "Intellect" },
+    -- Protection
+    [66] = { ArmorType = "Plate", Weapons = {}, PrimaryStat = "Strength" },
+    -- Retribution
+    [70] = { ArmorType = "Plate", Weapons = {}, PrimaryStat = "Strength" },
+    
+    -- Priest
+    -- Discipline
+    [256] = { ArmorType = "Cloth", Weapons = {}, PrimaryStat = "Intellect" },
+    -- Holy
+    [257] = { ArmorType = "Cloth", Weapons = {}, PrimaryStat = "Intellect" },
+    -- Shadow
+    [258] = { ArmorType = "Cloth", Weapons = {}, PrimaryStat = "Intellect" },
+    
+    -- Rogue
+    -- Assassination
+    [259] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Agility" },
+    -- Combat
+    [260] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Agility" },
+    -- Subtlety
+    [261] = { ArmorType = "Leather", Weapons = {}, PrimaryStat = "Agility" },
+    
+    -- Shaman
+    -- Elemental
+    [262] = { ArmorType = "Mail", Weapons = {}, PrimaryStat = "Intellect" },
+    -- Enhancement
+    [263] = { ArmorType = "Mail", Weapons = {}, PrimaryStat = "Agility" },
+    -- Restoration
+    [264] = { ArmorType = "Mail", Weapons = {}, PrimaryStat = "Intellect" },
+    
+    -- Warlock
+    -- Affliction
+    [265] = { ArmorType = "Cloth", Weapons = {}, PrimaryStat = "Intellect" },
+    -- Demonology
+    [266] = { ArmorType = "Cloth", Weapons = {}, PrimaryStat = "Intellect" },
+    -- Destruction
+    [267] = { ArmorType = "Cloth", Weapons = {}, PrimaryStat = "Intellect" },
+    
+    -- Warrior
+    -- Arms
+    [71] = { ArmorType = "Plate", Weapons = {}, PrimaryStat = "Strength" },
+    -- Fury
+    [72] = { ArmorType = "Plate", Weapons = {}, PrimaryStat = "Strength" },
+    -- Protection
+    [73] = { ArmorType = "Plate", Weapons = {}, PrimaryStat = "Strength" },
+}
